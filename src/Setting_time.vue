e375a99c0c69e6a7ec862b8a781b10e7ff9eb533
Zhu Yuan Hsiao <zhuyuanxiao0@gmail.com>
2020-09-24 18:52:09 +0800

    tomatoUI

--- src/Setting_time.vue
+++ src/Setting_time.vue
@@ -3,28 +3,28 @@
     <br/>
     <br/>
     <div>
-      <button id="add_study_minute" @click="study_minute_up">up</button>
-      <button id="add_study_second" @click="study_second_up">up</button>
+      <button class="buttonTimeSetting" id="add_study_minute" @click="study_minute_up"><img src="./assets/Vector up.png" width="24px" height= "24px"></button>
+      <button class="buttonTimeSetting" id="add_study_second" @click="study_second_up"><img src="./assets/Vector up.png" width="24px" height= "24px"></button>
     </div>
     <div>
       <p class="pSettingTime">專注時間 {{ Math.floor(study_time / 60) }} : {{ study_time % 60 }}</p>
     </div>
     <div>
-      <button id="Subtraction_study_minute" @click="study_minute_down">down</button>
-      <button id="Subtraction_study_second" @click="study_second_down">down</button>
+      <button class="buttonTimeSetting2" id="Subtraction_study_minute" @click="study_minute_down"><img src="./assets/Vector down.png" width="24px" height= "24px"></button>
+      <button class="buttonTimeSetting2" id="Subtraction_study_second" @click="study_second_down"><img src="./assets/Vector down.png" width="24px" height= "24px"></button>
     </div>
     <br/>
     <br/>
     <div>
-      <button id="add_relax_minute" @click="relax_minute_up">up</button>
-      <button id="add_relax_second" @click="relax_second_up">up</button>
+      <button class="buttonTimeSetting" id="add_relax_minute" @click="relax_minute_up"><img src="./assets/Vector up.png" width="24px" height= "24px"></button>
+      <button class="buttonTimeSetting" id="add_relax_second" @click="relax_second_up"><img src="./assets/Vector up.png" width="24px" height= "24px"></button>
     </div>
     <div>
       <p class="pSettingTime">休息時間 {{ Math.floor(relax_time / 60) }} : {{ relax_time % 60 }}</p>
     </div>
     <div>
-      <button id="Subtraction_relax_minute" @click="relax_minute_down">down</button>
-      <button id="Subtraction_relax_second" @click="relax_second_down">down</button>
+      <button class="buttonTimeSetting2" id="Subtraction_relax_minute" @click="relax_minute_down"><img src="./assets/Vector down.png" width="24px" height= "24px"></button>
+      <button class="buttonTimeSetting2" id="Subtraction_relax_second" @click="relax_second_down"><img src="./assets/Vector down.png" width="24px" height= "24px"></button>
     </div>
     <br/>
     <br/>
@@ -76,6 +76,27 @@ export default {
 
 <style>
 .pSettingTime {
-  font-size: 24px;
+  font-size: 30px;
+}
+.buttonTimeSetting {
+  position:relative;
+  left:65px;
+  background: none;
+  border: none;
+  box-shadow: none;
+  display: inline-block;
+  margin: 2px 4px;
+  cursor: pointer;
+}
+.buttonTimeSetting2 {
+  position:relative;
+  left:65px;
+  top:-25px;
+  background: none;
+  border: none;
+  box-shadow: none;
+  display: inline-block;
+  margin: 2px 4px;
+  cursor: pointer;
 }
 </style>
