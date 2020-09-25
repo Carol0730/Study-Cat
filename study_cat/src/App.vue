a89ff9a1904c891533f9b9553cb065852ed43a1d
JasonChang000 <jason0710727.eed07@nctu.edu.tw>
2020-09-25 23:07:17 +0800

    Init

--- study_cat/src/App.vue
+++ study_cat/src/App.vue
@@ -1,57 +1,50 @@
 <template>
   <div id="app">
-    <p><img src="./assets/Group 26.png" width="208" height="61"></p>
-<!--    <h1 style="text-align: center ">{{ text }}</h1>-->
-    <button class="button button3" id="task" @click="onLick">任務</button>
-    <button class="button button3" id="tomato" @click="onLick2">番茄鐘</button>
+    <h1 style="text-align: center ">{{ text }}</h1>
+    <button id="task" @click="onLick">任務</button>
+    <button id="tomato" @click="onLick2">番茄鐘</button>
     <br/>
     <br/>
     <div>
-      <select id="subject_lists" style="background:palevioletred;color:white;font-size:20px;">
-        <!--   <option v-for="s of subject_lists" :key="s" value="1">英文單字7000</option>-->
-        <!--        <option value="2">微積分</option>-->
+      <select id="subject_lists" v-model=the_target style="background:palevioletred;color:white;font-size:20px;" @change="change_target">
+<!--        <option v-for="(thing,index) of sl" :value="thing.name" :key="index"/>-->
+        <option v-for="(thing,index) of sl" :key="index">{{thing.name}}</option>
       </select>
     </div>
 
 
-<!--    番茄-->
     <div v-show="text2==='two'">
-      <div class="tomato-container">
-        <p class="p1">{{ Math.floor(timeRemaining / 60) }} : {{ timeRemaining % 60 }}</p>
-        <img alt="tomato" src="./assets/Group 29.png" width="300" height="300">
-        <p class="p2">{{ total_tomato }}</p>
-        <div>
-          <button class="buttonSetting" id="setting" @click="setting_window_click" v-show="text2!=='three'"><img src="./assets/Group 30.png" width="45px" height= "45px"></button>
-        </div>
+      <div>
+        <p>{{ Math.floor(timeRemaining / 60) }} : {{ timeRemaining % 60 }}</p>
+        <img alt="tomato" src="./assets/Group 25.png" width="300" height="300">
+        <p>{{ total_tomato }}</p>
       </div>
       <div>
-        <button class="buttonStart button3" id="start&stop" @click="start_study">{{ start_text }}</button>
+        <button id="start&stop" @click="start_study">{{ start_text }}</button>
       </div>
       <!--      <img alt="Vue logo" src="./assets/logo.png">-->
     </div>
-<!--    任務-->
     <div v-show="text2==='one'">
       <input type="text" id="new_subject" v-model=subject_name @keydown.enter="add_subject_into_list">
-<!--      <div>{{subject_name}}</div>-->
-<!--      <button id="subject_adding" @click="add_subject">add</button>-->
-<!--            <hr/>-->
-<!--            <span>-->
-<!--              <span style="text-align: center">英文單字7000</span>-->
-<!--              <img style="text-align: center" alt="tomato" src="./assets/Group 24.png" width="30" height="30">-->
-<!--              <span>{{ tomato }}</span>-->
-<!--            </span>-->
-<!--            <hr/>&lt;!&ndash;產生水平線 &ndash;&gt;-->
-      <list subjects = "英文單字7000" different_tomato = "0"></list>
-      <!--      <span style="text-align: left">微積分</span>-->
-      <!--      <img style="text-align: center" alt="tomato" src="./assets/Group 24.png" width="30" height="30">-->
-      <!--      <span>{{ tomato }}</span>-->
-      <!--      <hr/>-->
-      <!--      <img src="./assets/S__983043.jpg" height="500" width="500"/>-->
+      <ul>
+        <li v-for="(object,index) of sl" :key="index">
+          <hr/>{{object.name}}<img style="text-align: center" alt="tomato" src="./assets/Group 24.png" width="30" height="30">{{object.count}}
+          <span v-show="task_set===true"><button @click="change_number=index">修改</button><button @click="delete_task(index)">刪除</button></span>
+          <div v-show="change_number===index">
+            <input type="text" v-model=change_subject_name @keydown.enter="change_subject_name_in_list(index)">
+          </div>
+        </li>
+      </ul>
+      <hr/>
+      <button @click="task_setting">task_setting</button>
+      <br/>
+      <br/>
+
     </div>
     <setting_time :text2="text2" :study_time.sync="study_time" :relax_time.sync="relax_time"/>
-<!--    <div>-->
-<!--      <button class="buttonSetting" id="setting" @click="setting_window_click" v-show="text2!=='three'"><img src="./assets/Group 30.png" width="45px" height= "45px"></button>-->
-<!--    </div>-->
+    <div>
+      <button id="setting" @click="setting_window_click" v-show="text2!=='three'">setting</button>
+    </div>
 <!--    <div>-->
 <!--      <a href="https://www.youtube.com/?gl=TW&hl=zh-TW">This is a Link</a>-->
 <!--    </div>-->
@@ -62,7 +55,6 @@
 // import HelloWorld from './components/HelloWorld.vue'
 
 import setting_time from "./Setting_time";
-import Vue from 'vue'
 
 export default {
   name: 'App',
@@ -72,7 +64,7 @@ export default {
   },
   data() {
     return {
-      start_text: 'START',
+      start_text: 'start',
       text: 'Study Cat',
       text2: 'one',
       timeRemaining: 10,
@@ -80,14 +72,20 @@ export default {
       study_time: 10,
       relax_time: 3,
       total_tomato: 0,
-      tomato: 0,
+      // tomato: 0,
       study_timer: null,
       relax_timer: null,
-      subject_lists: [],
+      // ],{ name： '任務名稱' , }
+      sl : [],
       list_count: 0,
       subject_name: null,
       select: null,
-      option: null
+      option: null,
+      task_set: false,
+      the_target: null,
+      change_subject_name: null,
+      change_number: false,
+      original_tomato_index: -1
     }
   },
   methods: {
@@ -121,8 +119,9 @@ export default {
         this.timeRemaining = this.relax_time
         clearInterval(this.study_timer)
         this.relax_timer = setInterval(this.relax_starting, 1000)
-        this.tomato += 1
+        // this.tomato += 1
         this.total_tomato += 1
+        this.sl[this.original_tomato_index].count = this.total_tomato
       }
     },
     relax_starting() {
@@ -132,105 +131,49 @@ export default {
         clearInterval(this.relax_timer)
       }
     },
-    // add_subject() {
-    //   this.subject_name = document.getElementById("new_subject").value
-    //   console.log(this.subject_name)
-    //   this.select = document.getElementById("subject_lists")
-    //   this.select.add(this.subject_name, this.list_count)
-    //   this.list_count++
-    // },
     add_subject_into_list() {
-      console.log(this.subject_name)
-      this.select = document.getElementById("subject_lists")
-      this.option = document.createElement('option')
-      this.option.text = this.subject_name
-      this.select.add(this.option, this.list_count)
-      this.subject_lists[this.count] = this.subject_name
-      this.list_count++
-
+      // this.select = document.getElementById("subject_lists")
+      // this.option = document.createElement('option')
+      // this.option.text = this.subject_name
+      // this.select.add(this.option)
+      this.sl.push({
+        name: this.subject_name,
+        count: 0
+      })
+      // this.list_count++
+      this.subject_name = null
+    },
+    task_setting() {
+      this.task_set = !(this.task_set)
+    },
+    delete_task(index) {
+      this.sl.splice(index,1)
+      console.log(index,this.sl)
+      this.task_set = false
+    },
+    change_subject_name_in_list(index) {
+      this.sl[index].name = this.change_subject_name
+      this.change_number = -1
+      this.task_set = false
+    },
+    change_target() {
+      console.log(this.the_target)
+      for(let i = 0; i<this.sl.length; i++)
+      {
+        if(this.sl[i].name ===  this.the_target)
+        {
+          if(this.original_tomato_index !== -1)
+            this.sl[this.original_tomato_index].count = this.total_tomato
+          this.original_tomato_index = i
+          this.total_tomato = this.sl[i].count
+        }
+      }
     }
-
   }
 }
-Vue.component('list',{
-  template:'<hr/><span><span>{{subjects}}</span><img style="text-align: center" alt="tomato" src="./assets/Group 24.png" width="30" height="30"><span>{{different_tomato}}</span></span>',
-  props:['subjects,different_tomato']
-})
-// var vm = new Vue({
-//   el: '#app',
-//   data :{
-//     currentView : 'list'
-//   }
-// })
 </script>
 
 <style>
-.button {
-  background-color: #FBE9C6;
-  border: 2px solid #AB9872;
-  color: #76643E;
-  padding: 8px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 8px;
-  margin: 4px 10px;
-  cursor: pointer;
-  width: fit-content;
-  height: fit-content;
-  font-weight: bold;
-  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2), 0 0 0 0 rgba(0,0,0,0.19);
-}
-.buttonStart {
-  background-color: #FBE9C6;
-  border: 2px solid #AB9872;
-  color: #DB5D2B;
-  padding: 5px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 18px;
-  margin: 4px 10px;
-  cursor: pointer;
-  width: 100px;
-  height: min-content;
-  font-weight: bold;
-  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2), 0 0 0 0 rgba(0,0,0,0.19);
-}
-.buttonSetting {
-  position: absolute;
-  left:calc(50% + 50px);
-  background: none;
-  border: none;
-  box-shadow: none;
-  display: inline-block;
-  margin: 4px 10px;
-  cursor: pointer;
-  width: fit-content;
-  height: fit-content;
-}
-.button3 {border-radius: 16px;}
-
-.tomato-container {
-  position: relative;
-}
-
-.p1 {
-  position:absolute;
-  top: 55%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 36px;
-  color: white;
-}
-.p2 {
-  position: absolute;
-  left: calc(50% + 118px);
-  bottom: -5px;
-  transform: translateX(-50%);
-  font-size: 18px;
-  color: white;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -239,6 +182,5 @@ Vue.component('list',{
   color: #2c3e50;
   margin-top: 60px;
 }
-
 
 </style>
---
+++ study_cat/src/components/subject.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+    <hr/>
+    <span>
+    <span>{{ subjects }}</span>
+    <img style="text-align: center" alt="tomato" src="./assets/Group 24.png" width="30"
+         height="30">
+    <span>{{ different_tomato }}</span>
+  </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "subject",
+  props: ['subjects', 'different_tomato']
+}
+</script>
+
+<style scoped>
+
+</style>
