f296913cca0a259036dd85ca267d7e7730db2f6e
Zhu Yuan Hsiao <zhuyuanxiao0@gmail.com>
2020-09-29 01:07:18 +0800

    s

--- study_cat/src/TasksList.vue
+++ study_cat/src/TasksList.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
   div.container
-    a-list(:data-source="Object.values(projects)")
-      a-list-item(slot="renderItem" slot-scope="item")
-        a-list-item-meta
-          div(slot="title")
-            p(v-if="editingProjectName!==item.name" ) {{item.name}}
-            a-input(v-else v-model="editingProjectNewName" @keyup.enter="commitEdit") {{item.name}}
-        p(v-if="editingProjectName!==item.name")
-          button.buttonTaskSetting.buttonEdit(@click="setEditingProjects(item)") 修改
-          button.buttonTaskSetting.buttonDelete(@click="deleteProject(item)") 刪除
-        p(v-else)
-          button.buttonTaskSetting.buttonEdit(@click="commitEdit") 完成
+    div(v-for="item in Object.values(projects)")
+      a-space
+        div
+          span(v-if="editingProjectName!==item.name") {{item.name}}
+          input.input1(v-else v-model="editingProjectNewName" @keyup.enter="commitEdit")
+        div(v-if="editingProjectName!==item.name")
+          span {{item.pomodoro}}
+          button.buttonEdit(@click="setEditingProjects(item)") 修改
+          button.buttonDelete(@click="deleteProject(item)") 刪除
+        div(v-else)
+          button.buttonEdit(@click="commitEdit") 完成
 
 
-    p
+    div
       input.input(v-model="newProjectName" placeholder="想新增什麼科目呢？" @keyup.enter="addProjects")
-      a-button(@click="addProjects" icon="plus" shape="circle")
+      button.buttonEdit(@click="addProjects" icon="plus" shape="circle") ＋
 </template>
 <script>
 export default {
@@ -47,11 +47,13 @@ export default {
       this.editingProjectNewName = project.name
     },
     commitEdit() {
-      const newProjects = {...this.projects}
-      newProjects[this.editingProjectNewName] = newProjects[this.editingProjectName]
-      newProjects[this.editingProjectNewName].name = this.editingProjectNewName
-      delete newProjects[this.editingProjectName]
-      this.$emit('update:projects', newProjects)
+      if(this.editingProjectName !== this.editingProjectNewName){
+        const newProjects = {...this.projects}
+        newProjects[this.editingProjectNewName] = newProjects[this.editingProjectName]
+        newProjects[this.editingProjectNewName].name = this.editingProjectNewName
+        delete newProjects[this.editingProjectName]
+        this.$emit('update:projects', newProjects)
+      }
       this.editingProjectName = null
     },
     deleteProject(project){
@@ -64,25 +66,10 @@ export default {
 </script>
 
 <style>
-.ant-input{
-  width: 580px;
-  border-radius: 10px;
-  padding: 16px 16px !important;
-  margin: 8px 0 !important;
-}
-.input,select {
-  width: 40%;
-  height: 15px;
-  padding: 16px 16px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  box-sizing: border-box;
-}
-.buttonTaskSetting {
+
+.buttonAdd{
   position: relative;
-  right:10%;
+  right:0;
   display: inline-block;
   cursor: pointer;
   width: 50px;
@@ -96,13 +83,66 @@ export default {
   text-decoration: none;
   font-size: 14px;
   box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2), 0 0 0 0 rgba(0,0,0,0.19);
+  background-color:#7F9BD2;
 }
 
+.input {
+  width: 40%;
+  height: 15px;
+  padding: 16px 16px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-sizing: border-box;
+}
+.input1 {
+  width: 40%;
+  height: 15px;
+  padding: 16px 14px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-sizing: border-box;
+}
+.task-container{
+  position:relative;
+}
 .buttonEdit{
+  position: relative;
+  right:0;
+  display: inline-block;
+  cursor: pointer;
+  width: 50px;
+  height: 30px;
+  border-radius: 10px;
+  color:white;
+  border: none;
+  padding: 5px;
+  margin: 2px 4px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 14px;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2), 0 0 0 0 rgba(0,0,0,0.19);
   background-color:#7F9BD2;
 }
 .buttonDelete{
+  position: relative;
+  right:0;
+  display: inline-block;
+  cursor: pointer;
+  width: 50px;
+  height: 30px;
+  border-radius: 10px;
+  color:white;
+  border: none;
+  padding: 5px;
+  margin: 2px 4px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 14px;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2), 0 0 0 0 rgba(0,0,0,0.19);
   background-color:#EB8D8D;
 }
-
 </style>
