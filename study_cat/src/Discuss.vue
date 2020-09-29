311240ee2f97790920009a3dd464de8dbc37033b
solrz <kbpsolrz@gmail.com>
2020-09-29 19:36:42 +0800

    Add discuss page

---
+++ study_cat/src/Discuss.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  div
+    a-dropdown.w-50.p-3
+      a.ant-dropdown-link {{browsingSubject}}
+        a-icon(type="down")
+      a-menu(v-for="p in Object.values(projects)" :key="p.name"  slot="overlay")
+        a-menu-item(:value="p.name" @click="browsingSubject=p.name") {{p.name}}
+    a-card
+      span wow
+
+</template>
+<script>
+export default {
+  name: 'discuss',
+  props: {
+    projects: {}
+  },
+  data(){
+    return{
+      browsingSubject: '無任務'
+    }
+  }
+}
+</script>
