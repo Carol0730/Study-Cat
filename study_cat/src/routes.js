684711ea2a8799e341016a11ef78134bdc0248ed
solrz <kbpsolrz@gmail.com>
2020-09-30 20:01:22 +0800

    Fix gh-pages don't support html5 history mode.

--- study_cat/src/routes.js
+++ study_cat/src/routes.js
@@ -26,6 +26,5 @@ const routes = [
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 export const router = new VueRouter({
-    mode: 'history',
     routes // (缩写) 相当于 routes: routes
 })
